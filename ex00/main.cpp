67324baf64f2a224ba4e00a4da5ed06c2042bc66
ArthurBauu <arthurbaudouin05@gmail.com>
2026-01-18 15:14:37 +0100

    Add files via upload

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,36 +2,46 @@
 
 int main() {
 
+	// Test avec un conteneur modifiable
 	std::vector<int> vec;
 	for (int i = 0; i < 10; ++i) {
 		vec.push_back(i * 10);
 	}
+
+	// Test avec une valeur présente
 	try {
 		std::vector<int>::iterator it = easyfind(vec, 50);
 		std::cout << "Found value: " << *it << std::endl;
 	} catch (const std::runtime_error &e) {
 		std::cerr << e.what() << std::endl;
 	}
+	// Test avec une valeur absente
 	try {
 		std::vector<int>::iterator it = easyfind(vec, 55);
 		std::cout << "Found value: " << *it << std::endl;
 	} catch (const std::runtime_error &e) {
 		std::cerr << e.what() << std::endl;
 	}
+	// Modification du conteneur via l'itérateur retourné
+	try {
+        std::vector<int>::iterator it = easyfind(vec, 50);
+        std::cout << "Found value: " << *it << std::endl;
+        *it = 999;  // Possible car iterator non-const
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
-	// const std::vector<int> constVec = vec;
-	// try {
-	// 	std::vector<int>::const_iterator it = easyfind(const_cast<std::vector<int>&>(constVec), 30);
-	// 	std::cout << "Found value in const container: " << *it << std::endl;
-	// } catch (const std::runtime_error &e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	std::vector<int>::const_iterator it = easyfind(const_cast<std::vector<int>&>(constVec), 35);
-	// 	std::cout << "Found value in const container: " << *it << std::endl;
-	// } catch (const std::runtime_error &e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+
+	
+	// Test avec un conteneur constant
+	const std::vector<int> constVec = vec;
+    try {
+        std::vector<int>::const_iterator it = easyfind(constVec, 30);
+        std::cout << "Found value in const container: " << *it << std::endl;
+        // *it = 999;  // Erreur de compilation : const_iterator ne permet pas la modification
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
 
 
